552ce72364a5e3c9cecde398640e49e66db7e7da
valeriiaradzivilo <v.radzivilo.ip14@kpi.ua>
2023-07-18 10:43:55 +0300

    fix test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -8,16 +7,17 @@ import 'package:lingui_quest/shared/widgets/lin_main_button.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('test-sign-up', () async {
-    await Firebase.initializeApp();
+  group('test-sign-up', () {
     testWidgets('Authentication Testing', (WidgetTester tester) async {
+      // await Firebase.initializeApp();
       await tester.pumpWidget(const MyApp());
       await tester.pumpAndSettle();
+      await tester.tap(find.byKey(const ValueKey('avatarIcon')));
       await tester.enterText(
           find.byKey(const ValueKey('emailSignUpField')), 'test@zip.com');
       await tester.enterText(
           find.byKey(const ValueKey('passwordSignUpField')), 'test');
-      await addDelay(1000);
+      await addDelay(10);
       expect(
           tester
               .widget<LinMainButton>(find.byKey(const ValueKey('signUpButton')))
